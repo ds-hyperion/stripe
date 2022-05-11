7edf3523399b891194597ae73d3936ae4e995e06
Grégory COLLIN <gregory@d4rk0s.net>
2022-05-11 13:48:24 +0200

    Adding subscription and some bugfixes

--- src/Enum/StripeEventEnum.php
+++ src/Enum/StripeEventEnum.php
@@ -5,4 +5,5 @@ namespace Hyperion\Stripe\Enum;
 enum StripeEventEnum : string
 {
     case PAYMENT_SUCCESS = 'payment_intent.succeeded';
+    case SETUPINTENT_SUCCESS = 'setup_intent.succeeded';
 }
---
+++ src/Service/SubscriptionService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Hyperion\Stripe\Service;
+
+use D4rk0snet\Donation\Enums\DonationRecurrencyEnum;
+use Stripe\Customer;
+
+class SubscriptionService extends StripeService
+{
+
+    public static function createSubscription(string $customerEmail,
+                                              string $firstname,
+                                              string $lastname,
+                                              string $paymentMethodId,
+                                              float $amount)
+    {
+        /** @var Customer $customer */
+        $customer = CustomerService::getCustomerIdByEmail($customerEmail);
+        if($customer === null) {
+            $customer = CustomerService::createCustomer(
+                email: $customerEmail,
+                firstName: $firstname,
+                lastName: $lastname,
+                metadata: ['type' => 'individual']
+            );
+        }
+
+        self::getStripeClient()->paymentMethods->attach($paymentMethodId, [
+            'customer' => $customer->id
+        ]);
+
+        self::getStripeClient()->customers->update($customer->id, [
+            'invoice_settings' => [
+                'default_payment_method' => $paymentMethodId
+            ]
+        ]);
+
+        $price = self::getStripeClient()->prices->create([
+            'unit_amount' => $amount * 100,
+            'currency' => 'eur',
+            'recurring' => ['interval' => 'month'],
+            'product' => DonationRecurrencyEnum::MONTHLY->getStripeProductId()
+        ]);
+
+        $subscription = self::getStripeClient()->subscriptions->create(
+            [
+                'customer' => $customer->id,
+                'items' => [
+                    'price' => $price->id
+                ],
+                'expand' => ['latest_invoice.payment_intent']
+            ]
+        );
+    }
+}
