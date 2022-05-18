0865d835c7ece5c684613e67e437b6ae42997c4c
d4rk0s <gregory@d4rk0s.net>
2022-05-18 19:28:44 +0200

    Gestion des entreprises

--- src/Service/CustomerService.php
+++ src/Service/CustomerService.php
@@ -27,7 +27,7 @@ class CustomerService extends StripeService
     }
 
 
-    public static function getOrCreateCustomer(
+    public static function getOrCreateIndividualCustomer(
         string $email,
         string $firstName,
         string $lastName,
@@ -44,7 +44,34 @@ class CustomerService extends StripeService
             [
                 'email' => strtolower($email),
                 'name' => trim(ucfirst(strtolower($firstName)))." ".trim(strtoupper($lastName)),
-                'metadata' => $metadata
+                'metadata' => array_merge(['type' => 'individual'], $metadata)
+            ]
+        );
+
+        self::$customers[md5($email)] = $customer;
+
+        return $customer;
+    }
+
+
+    public static function getOrCreateCompanyCustomer(
+        string $email,
+        string $companyName,
+        string $mainContactName,
+        array $metadata = []
+    ) : Customer {
+        $client = self::getStripeClient();
+
+        $customer = self::getCustomerIdByEmail($email);
+        if ($customer !== null) {
+            return $customer;
+        }
+
+        $customer = $client->customers->create(
+            [
+                'email' => strtolower($email),
+                'name' => trim(ucfirst($mainContactName))." - $companyName",
+                'metadata' => array_merge(['type' => 'company'], $metadata)
             ]
         );
 
