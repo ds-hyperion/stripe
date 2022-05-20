63abcc97acd3c6eb9d9bd7e5bf2c69ea58ac8910
d4rk0s <gregory@d4rk0s.net>
2022-05-20 10:24:17 +0200

    Bugfixes

--- src/Service/CustomerService.php
+++ src/Service/CustomerService.php
@@ -17,10 +17,8 @@ class CustomerService extends StripeService
 
         $client = self::getStripeClient();
         $customerCollection = $client->customers->all(['email' => strtolower($email)]);
-        if (count($customerCollection) > 0) {
-            self::$customers[$emailMd5] = current($customerCollection)->data;
-
-            return self::$customers[$emailMd5];
+        if (count($customerCollection->data) > 0) {
+            return current($customerCollection->data);
         }
 
         return null;
